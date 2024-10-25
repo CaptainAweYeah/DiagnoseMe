e58b0c6b484fc47fd8ed398605b37a6ebadf02ec
CaptainAweYeah <ashmorewillard@gmail.com>
2024-10-25 18:21:13 -0500

    Initial Upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace DiagnoseMe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            outText.Text = GetIP().ToString();
+
+            if (clientCode.AcceptsReturn)
+            {
+                if (IPAddress.TryParse(clientCode.Text, out IPAddress ip))
+                {
+                    ServerLink serverLink = new ServerLink();
+                    Task<string> inputText = serverLink.Server(ip);
+
+                    string inTo = inputText.Result.ToString();
+                    outText.Text = inTo;
+                }
+            }
+            
+            
+        }
+
+        public static IPAddress GetIP()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip;
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+
+    }
+}
---
+++ server.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagnoseMe
+{
+    public class ServerLink
+    {
+        async internal Task<string> Server(IPAddress ipAddress)
+        {
+            IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+
+            using Socket listener = new(
+                ipEndPoint.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            listener.Bind(ipEndPoint);
+            listener.Listen(100);
+
+            var handler = await listener.AcceptAsync();
+            while (true)
+            {
+                // Receive message.
+                var buffer = new byte[1_024];
+                var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+                var eom = "<|EOM|>";
+                if (response.IndexOf(eom) > -1 /* is end of message */)
+                {
+                    return response;
+                }
+            }
+        }
+    }
+}
